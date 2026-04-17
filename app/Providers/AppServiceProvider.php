1537e1dced006695515c4aa155c1c1fef069e3e2
devifyo <sunilthagyal355@gmail.com>
2026-04-17 16:58:26 +0000

    fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,24 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // Force it unconditionally just to test
         URL::forceScheme('https');
+
+        Event::listen(SocialiteWasCalled::class, [
+            \SocialiteProviders\Apple\AppleExtendSocialite::class, 'handle',
+        ]);
     }
 }
